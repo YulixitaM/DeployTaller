ccbf4ddafcf7b00987001ab641c4b6948d8156de
Yuli Martinez <yulimartinez@unicomfacauca.edu.co>
2021-10-22 15:48:14 -0500

    Cambio detectado

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
+# importar librerias de flash 
+# from types import MethodDescriptorType
+# import joblib
 from flask import Flask, request
 import pickle
 import numpy as np
 
 app = Flask(__name__)
-@app.route("/")
+
+@app.route('/')
 def home():
     return 'Deploy ML'
 
-@app.route('/predecir',methods=['POST'])
+# Crear funcion para predecir 
+@app.route('/predecir', methods=['POST'])
 def prediccion():
     json = request.get_json(force=True)
     xin = json['Datos']
-    print(xin)
+    xin = np.asarray(xin)
+    xin = xin.reshape(1,13)
+    # print("Servicio post")
     yout = model.predict(xin)
     print(yout)
-    mensaje = '' 
+    mensaje = ''
     for y_out in yout:
-        mensaje = mensaje + 'El paciente corresponde a la clase {} /n'.format(labels[y_out])
+        mensaje = mensaje + 'El paciente se encuentra :' + labels[y_out]
     
-    return mensaje 
+    return mensaje
 
-pkl_filename='Heart_model.pkl'
-with open(pkl_filename,'rb')as file:
+
+
+# Cargar modelo
+pkl_filename = 'Heart_model.pkl'
+with open(pkl_filename, 'rb') as file:
     model = pickle.load(file)
+    
+labels = ['Sano', 'Enfermo'] # Etiquetas de datos
 
-labels =['Sano', 'Enfermo']    
-
-if __name__ =='__main__':
-   app.run()
+if __name__ == '__main__':
+    app.run()
